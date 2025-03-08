33e8de8586f9908a599e9b7668da806e9cee2e84
ANDRESCELEITA <andrescaceres9301@gmail.com>
2025-03-08 15:23:31 -0500

    se creo una variable en numeromayor.php

--- numeromayor.php
+++ numeromayor.php
@@ -4,6 +4,7 @@ $numero1 = 5000;
 $numero2 = 30000;
 $numero3 = 1000;
 
+$numero5 =0;
 if ($numero1 >= $numero2 and $numero1 >= $numero3){
     echo ("El numero ". $numero1 . " es mayor que el numero ". $numero2 . " y el numero " . $numero3. ".");
 }else if ($numero2 >= $numero3) {
